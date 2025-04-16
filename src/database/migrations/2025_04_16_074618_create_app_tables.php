d540847e3a2e50250676603b5fa49dea76d35a7e
r <142658858+pianomachine@users.noreply.github.com>
2025-04-16 17:14:08 +0900

    deploy.yml変更13

---
+++ src/database/migrations/2025_04_16_074618_create_app_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_tables', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+        Schema::create('primary_categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_tables');
+        Schema::dropIfExists('primary_categories');
+    }
+}
