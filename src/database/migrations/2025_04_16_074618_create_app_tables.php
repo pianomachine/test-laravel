db90d48ab8ce744f40dbb75dd5be049fd707de9e
r <142658858+pianomachine@users.noreply.github.com>
2025-04-16 18:57:21 +0900

    deploy.yml変更14

--- src/database/migrations/2025_04_16_074618_create_app_tables.php
+++ src/database/migrations/2025_04_16_074618_create_app_tables.php
@@ -22,6 +22,41 @@ class CreateAppTables extends Migration
 
             $table->timestamps();
         });
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('primary_category_id');
+
+            // ここにカラムを追加していく
+
+            $table->timestamps();
+
+            $table->foreign('primary_category_id')->references('id')->on('primary_categories');
+        });
+
+         Schema::create('item_conditions', function (Blueprint $table) {
+             $table->id();
+ 
+             // ここにカラムを追加していく
+ 
+             $table->timestamps();
+         });
+ 
+         Schema::create('items', function (Blueprint $table) {
+             $table->id();
+             $table->unsignedBigInteger('seller_id');
+             $table->unsignedBigInteger('buyer_id');
+             $table->unsignedBigInteger('secondary_category_id');
+             $table->unsignedBigInteger('item_condition_id');
+ 
+             // ここにカラムを追加していく
+ 
+             $table->timestamps();
+ 
+             $table->foreign('seller_id')->references('id')->on('users');
+             $table->foreign('buyer_id')->references('id')->on('users');
+             $table->foreign('secondary_category_id')->references('id')->on('secondary_categories');
+             $table->foreign('item_condition_id')->references('id')->on('item_conditions');
+         });
     }
 
     /**
@@ -32,6 +67,8 @@ class CreateAppTables extends Migration
     public function down()
     {
         Schema::dropIfExists('app_tables');
+        Schema::dropIfExists('primary_categories');
+        Schema::dropIfExists('secondary_categories');
         Schema::dropIfExists('primary_categories');
     }
 }
